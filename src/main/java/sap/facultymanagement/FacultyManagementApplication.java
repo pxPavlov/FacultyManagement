93c560f98ff1ec1e04367f9ff52f84bf9446e9aa
Pavel Pavlov <p.h.pavlov@gmail.com>
2020-12-15 13:59:05 +0200

    First commit

---
+++ src/main/java/sap/facultymanagement/FacultyManagementApplication.java
@@ -0,0 +1,13 @@
+package sap.facultymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacultyManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sap/facultymanagement/FacultyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package sap.facultymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
